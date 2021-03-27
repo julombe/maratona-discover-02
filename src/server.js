560d8d6b46e6f32e6d2182a75e0a077127abb21b
julombe <jose.ulombe@hotmail.com>
2021-03-27 15:02:27 +0100

    initial commit

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const server = express()
 const routes = require("./routes")
 
 
+//configuring template engine
+server.set('view engine', 'ejs')
+
 // enable css. Static files
 server.use(express.static('public'))
 
